5ed40a45d244d4db5cec9854efbf10f469e77f24
Joshua-Shockley <thelumbashockley@gmail.com>
2020-01-28 20:41:05 -0600

    getting through lru cache

--- lru_cache/doubly_linked_list.py
+++ lru_cache/doubly_linked_list.py
@@ -11,6 +11,7 @@ class ListNode:
     """Wrap the given value in a ListNode and insert it
     after this node. Note that this node could already
     have a next node it is point to."""
+
     def insert_after(self, value):
         current_next = self.next
         self.next = ListNode(value, self, current_next)
@@ -20,6 +21,7 @@ class ListNode:
     """Wrap the given value in a ListNode and insert it
     before this node. Note that this node could already
     have a previous node it is point to."""
+
     def insert_before(self, value):
         current_prev = self.prev
         self.prev = ListNode(value, current_prev, self)
@@ -28,7 +30,8 @@ class ListNode:
 
     """Rearranges this ListNode's previous and next pointers
     accordingly, effectively deleting this ListNode."""
-    def delete(self):
+
+    def delete(self):  # sets the new pointers thus removing from list
         if self.prev:
             self.prev.next = self.next
         if self.next:
@@ -41,33 +44,89 @@ the list's head and tail nodes."""
 
 class DoublyLinkedList:
     def __init__(self, node=None):
-        self.head = node
-        self.tail = node
-        self.length = 1 if node is not None else 0
+        self.head = node  # creates the head position sets to None as default
+        self.tail = node  # creates the tail position sets to None as defalut
+        # sets the length method/prop to value of 0 if nothing other wise one item makes length of 1
+        self.length = 1 if node is not None else 0  # done as a list comprehension
 
     def __len__(self):
-        return self.length
+        return self.length  # allows us to call len() as a fn
 
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if not self.head and not self.tail:  # asks if there is no head and tail meaning empty
+            self.head = new_node  # sets the new node to head
+            self.tail = new_node  # sets the new node to be the tail also since its the only node
+        else:  # meaning so if there is a length to the DLL 1 or more
+            # the old head is set to the next position pointer from new node
+            new_node.next = self.head
+            # the new node is now set pointer that new node is prev to original head
+            self.head.prev = new_node
+            self.head = new_node  # sets the new node to the head position
+        return new_node
 
     def remove_from_head(self):
-        pass
+        # sets the variable so it can be returned using node's value method
+        value = self.head.value
+        # performs the delete from below and sets the return value as self.head
+        self.delete(self.head)
+        return value  # actually returns the value to end the fn
 
     def add_to_tail(self, value):
-        pass
+        # creates the instance of the node
+        new_node = ListNode(value, None, None)
+        self.length += 1  # adds to the length prop in ListNode
+        if not self.head and not self.tail:  # if list is empty
+            self.head = new_node  # makes the only new value head and below tail also
+            self.tail = new_node
+        # this sets up the new pointer for the new last item since there  is 1+ item(s)
+        else:
+            new_node.prev = self.tail  # sets og tail to being the new node's now prev
+            self.tail.next = new_node  # sets new nod to be the og tail's new next pointer
+            self.tail = new_node  # then sets the new node to be the tail
 
     def remove_from_tail(self):
-        pass
+        value = self.tail.value  # sets the value fo the current tail to a var
+        self.delete(self.tail)  # deletes the og tail
+        return value  # returns the value of the deleted tail node
 
     def move_to_front(self, node):
-        pass
+        value = node.value  # sets the value of the node to move to var
+        self.delete(node)  # deletes the og node (delete redoes pointers)
+        self.add_to_head(value)  # adds the var value to the head position
 
     def move_to_end(self, node):
-        pass
+        value = node.value  # sets the value of the nod to move to var
+        self.delete(node)  # deltes the node (delete handles pointers)
+        self.add_to_tail(value)  # adds the value to tail postion
 
     def delete(self, node):
-        pass
+        self.length -= 1
+        # if empty
+        if not self.head and not self.tail:
+            return None  # return that there is nothing
+        # if head and tail
+        if self.head == self.tail:
+            self.head = None  # sets the value to none since there was only length=1 before delete
+            self.tail = None  # same as line above but for tail
+        # if the head
+        elif self.head == node:  # if the lenght is more than one and deleting the head
+            self.head = self.head.next  # sets the position for second item to be first
+            node.delete()  # then deletes the node picked
+        elif self.tail == node:  # if the lenght is more than one and deleting the tail
+            self.tail = self.tail.prev  # sets the prev to tail to be new tail
+            node.delete()  # deletes the og tail listNode delete fn handles the reassignement of the new prev and next
+        else:
+            node.delete()
 
     def get_max(self):
-        pass
+        if self.head is None:
+            return None
+        max_value = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,8 @@
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('../doubly_linked_list')
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +11,18 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.stuff = {}
+        self.length = 0
+        self.dll = DoublyLinkedList()
+
+    def __ln__(self):
+        return self.length
+
+    def __str__(self):
+        return self.stuff
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +31,18 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        try:
+            # it exists
+            # value = self.stuff[key]
+            # node = (key, value)
+            node = self.stuff[key]
+            self.dll.move_to_front(node)  # this moves to front of dll
+            return node.value[1]
+        except:
+            # doesn't exist
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +54,74 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
-    def set(self, key, value):
-        pass
+
+    def set(self, key, value):  # cache is called stuff
+        try:
+            # It does exist
+            node = self.stuff[key]
+            node.value = (key, value)
+            return node.value[1]
+        except:
+            # It does not exist
+            print('makes it into: It does not exist part of set fn')
+            self.length += 1
+            # this is the attempt to rewire
+            # this sets node for adding to dll
+            node = (key, value)
+            # node = self.stuff[key]
+            self.dll.add_to_head(node)  # adds node to dll at the head
+            self.stuff[key] = self.dll.add_to_head(
+                node)  # adds key to the cache
+            nv = self.stuff[key]
+
+            while self.length > self.limit:
+                tail = self.dll.tail
+                key_to_go = tail.value[0]
+                print('print tail', key_to_go)
+                self.dll.remove_from_tail()
+                # end of actually removing item past limit
+                # deletes the least used value from cache
+                del self.stuff[key_to_go]
+                self.length -= 1
+                print(self.length)
+
+            return nv.value[1]
+# this is the end of the atttempt rewire
+
+        #     print(self.length)
+        #     while self.length > self.limit:  # removes item past limit
+        #         tail = self.dll.tail
+        #         key_to_go = tail.value[0]
+        #         print('print tail', key_to_go)
+        #         self.dll.remove_from_tail()
+        #         # end of actually removing item past limit
+        #         # deletes the least used value from cache
+        #         del self.stuff[key_to_go]
+        #         self.length -= 1
+        #         print(self.length)
+        # finally:
+        #     # this sets node for adding to dll
+        #     node = (key, value)
+        #     # node = self.stuff[key]
+        #     self.dll.add_to_head(node)  # adds node to dll at the head
+        #     self.stuff[key] = self.dll.add_to_head(
+        #         node)  # adds key to the cache
+        #     nv = self.stuff[key]
+        #     return nv.value[1]
+
+
+test = LRUCache(3)
+test.set('item1', '1')
+test.set('item2', '2')
+test.set('item3', '3')
+test.set('item2', 're-set')
+print(test.get('item2'))
+
+print(test.get('item1'))
+print(test.get('item3'))
+print(test.set('item4', '4'))
+print('test should come back None:', test.get('item2'))
+print('test should come back with value of item1:', test.get('item1'))
+print(test.limit)
+print(len(test.stuff))
+print(test.length)
